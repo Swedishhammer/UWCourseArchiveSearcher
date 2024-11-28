4d8652bc34a08aff693a3b614d93c85c4539d881
Oskar <oskarcole@gmail.com>
2024-11-28 01:29:34 -0800

    Update CourseParser.py
    CourseParser successfully grabs course titles from Time Schedule but needs manual setup for each Time Schedule

--- CourseParser.py
+++ CourseParser.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import json
 import os
 
-HTML_SOURCE_DIR = "UWCourseArchiveSearcher/TimeSchedules"
+HTML_SOURCE_DIR = "TimeSchedules"
 OUTPUT_FILE = "CourseData.json"
 
 def parseHTMLFile(filePath):
@@ -12,12 +12,18 @@ def parseHTMLFile(filePath):
     
     titles = [h1.text.strip() for h1 in dataSoup.find_all('h1')]
     paragraphs = [p.text.strip() for p in dataSoup.find_all('p')]
+    #tables = [table.text.strip() for table in dataSoup.find_all('table')]
+    tables = []
+    for table in dataSoup.find_all('table'):
+        text = table.text.strip()
+        if text.startswith('CSE'):
+            tables.append(text)
 
-    return {"titles": titles, "paragraphs": paragraphs}
+    return {"titles": titles, "paragraphs": paragraphs, "tables": tables}
     
 
 def saveToJSON(data):
-    with open(OUTPUT_FILE, 'w', econding='utf-8') as file:
+    with open(OUTPUT_FILE, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
         print(f"Data saved to {OUTPUT_FILE}")
 
