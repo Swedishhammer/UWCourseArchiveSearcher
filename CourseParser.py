e1afb7eefedc7aed8dc0c274ab61776c4b30883c
Oskar <oskarcole@gmail.com>
2024-11-30 01:43:51 -0800

    Update CourseParser.py
    Now parses all files in provided directory, added course name trimming to improve look

--- CourseParser.py
+++ CourseParser.py
@@ -3,22 +3,33 @@ import json
 import os
 
 HTML_SOURCE_DIR = "TimeSchedules"
-OUTPUT_FILE = "CourseData.json"
+OUTPUT_FILE = "CourseDataTest.json"
 
+def courseNameTrim(text):
+    parenIndex = text.find("(")
+    if (parenIndex != -1):
+        text = text[0 : parenIndex]
+    
+    prereqIndex = text.find("Prerequisites")
+    if (prereqIndex != -1):
+        text = text[0 : prereqIndex]
+    return text
 def parseHTMLFile(filePath):
     print(f"Parsing: {filePath}")
     with open(filePath, 'r', encoding='utf-8') as file:
         dataSoup = BeautifulSoup(file, 'lxml')
-    
+    fileName = filePath[(filePath.rindex('/') + 1) : len(filePath)]
+    deptCode = fileName[0 : fileName.index("_")]
+    print(f"deptCode is {deptCode}")
     titles = [h1.text.strip() for h1 in dataSoup.find_all('h1')]
-    paragraphs = [p.text.strip() for p in dataSoup.find_all('p')]
     tables = []
     for table in dataSoup.find_all('table'):
         text = table.text.strip()
-        if text.startswith('CSE'):
+        if text.startswith(deptCode):
+            text = courseNameTrim(text)
             tables.append(text)
 
-    return {"titles": titles, "paragraphs": paragraphs, "tables": tables}
+    return {"titles": titles, "tables": tables}
     
 
 def saveToJSON(data):
@@ -26,7 +37,16 @@ def saveToJSON(data):
         json.dump(data, file, ensure_ascii=False, indent=4)
         print(f"Data saved to {OUTPUT_FILE}")
 
+def parseAllHTMLFiles():
+    parsedData = []
 
-filePath = HTML_SOURCE_DIR + "/CSE_24AU.html"
-parsedData = parseHTMLFile(filePath)
+    for fileName in os.listdir(HTML_SOURCE_DIR):
+        if fileName.endswith('.html'):
+            filePath = HTML_SOURCE_DIR + "/" + fileName
+            parsedData.append(parseHTMLFile(filePath))
+    return parsedData
+#filePath = HTML_SOURCE_DIR + "/CSE_24AU.html"
+#filePath = HTML_SOURCE_DIR + "/Latin Sucess"
+parsedData = parseAllHTMLFiles()
+print(f"data: {parsedData}")
 saveToJSON(parsedData)
